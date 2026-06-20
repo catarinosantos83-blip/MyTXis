cf1fb439d099e2e2973193f1ce893973baede061
catarinosantos83-blip <catarinosantos83@gmail.com>
2026-06-19 22:25:28 -0600

    ERRORES DE CAPTURA DE DATOS
    REVISION DE LOS EDITEX  Y EN ACTIVITY REGISTRO CONDUCTOR SE GUARDEN LOS DATOS EN AUTOMATICO

--- app/src/main/java/com/cibergoliath/mytxis/RegistroConductorActivity.java
+++ app/src/main/java/com/cibergoliath/mytxis/RegistroConductorActivity.java
@@ -9,11 +9,22 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import android.widget.Button;
 
 public class RegistroConductorActivity extends AppCompatActivity {
 
     Button btnIngresar, btnVolver;
+
+    Button btnDatos;
+
+    EditText txtPlaca;
+    EditText txtMarca;
+    EditText txtModelo;
+    EditText txtColor;
 
 
     @Override
@@ -24,6 +35,57 @@ public class RegistroConductorActivity extends AppCompatActivity {
 
         btnIngresar = findViewById(R.id.btnIngresar);
         btnVolver = findViewById(R.id.btnVolver);
+
+        btnDatos = findViewById(R.id.btnDatos);
+
+        txtPlaca = findViewById(R.id.txtPlaca);
+        txtMarca = findViewById(R.id.txtMarca);
+        txtModelo = findViewById(R.id.txtModelo);
+        txtColor = findViewById(R.id.txtColor);
+
+        SharedPreferences preferencias =
+                getSharedPreferences("conductor", MODE_PRIVATE);
+
+        txtPlaca.setText(
+                preferencias.getString("placa", ""));
+
+        txtMarca.setText(
+                preferencias.getString("marca", ""));
+
+        txtModelo.setText(
+                preferencias.getString("modelo", ""));
+
+        txtColor.setText(
+                preferencias.getString("color", ""));
+
+        btnDatos.setOnClickListener(v -> {
+
+
+
+            SharedPreferences.Editor editor =
+                    preferencias.edit();
+
+            editor.putString("placa",
+                    txtPlaca.getText().toString());
+
+            editor.putString("marca",
+                    txtMarca.getText().toString());
+
+            editor.putString("modelo",
+                    txtModelo.getText().toString());
+
+            editor.putString("color",
+                    txtColor.getText().toString());
+
+            editor.apply();
+
+            Toast.makeText(
+                    this,
+                    "Datos guardados correctamente",
+                    Toast.LENGTH_SHORT
+            ).show();
+
+        });
 
         btnIngresar.setOnClickListener(v -> {
 
